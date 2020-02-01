d64e637d4e38cdeaa1f8c56316d899a27dcd27d3
Rodrigo Portilio <rodrigo.portilio@outlook.com>
2020-02-01 15:04:25 -0300

    Add project files.

---
+++ ReviewNetCore.ConsoleApplicationEFCoreScaffold/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReviewNetCore.ConsoleApplicationEFCoreScaffold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
